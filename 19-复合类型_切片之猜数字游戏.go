6d92c19675c14e085413ec28d8c91e0ebbdc8442
gaojian <gaojian_1010@126.com>
2019-02-01 10:05:12 +0800

    猜数字游戏完善

--- 19-复合类型_切片之猜数字游戏.go
+++ 19-复合类型_切片之猜数字游戏.go
@@ -21,15 +21,10 @@ func CreatNum(randNum *int) {
 
 // 将数字转换成长度为4的slice
 func GetNum(s []int, randNum int) {
-
-	s[0] = randNum / 1000 // 取千位数
-	fmt.Println("千位数：", s[0])
+	s[0] = randNum / 1000       // 取千位数
 	s[1] = randNum % 1000 / 100 // 取百位数
-	fmt.Println("百位数：", s[1])
-	s[2] = randNum % 100 / 10 // 取十位数
-	fmt.Println("十位数：", s[2])
-	s[3] = randNum % 10 // 取十位数
-	fmt.Println("个位数：", s[3])
+	s[2] = randNum % 100 / 10   // 取十位数
+	s[3] = randNum % 10         // 取十位数
 }
 
 // 输入一个4位数并取出每一位
@@ -39,29 +34,29 @@ func OneGame(s []int) {
 		fmt.Println("请输入一个4位数字：")
 		fmt.Scan(&inum)
 		if 999 < inum && inum < 10000 {
-			break
-		}
-		fmt.Printf("输入的数字不符合要求，请重新输入 \n")
-	}
-	fmt.Printf("输入的数字是：%d", inum)
-	k := make([]int, 4)
-	GetNum(k, inum)
-	fmt.Println("KeySlice", k)
+			fmt.Printf("输入的数字是：%d \n", inum)
 
-	n := 0
-	for i := 0; i < 4; i++ {
-		if k[i] > s[i] {
-			fmt.Printf("第%d位数大了一点\n", i+1)
-		} else if k[i] < s[i] {
-			fmt.Printf("第%d位数小了一点\n", i+1)
+			k := make([]int, 4)
+			GetNum(k, inum)
+
+			n := 0
+			for i := 0; i < 4; i++ {
+				if k[i] > s[i] {
+					fmt.Printf("第%d位数大了一点\n", i+1)
+				} else if k[i] < s[i] {
+					fmt.Printf("第%d位数小了一点\n", i+1)
+				} else {
+					fmt.Printf("第%d位数猜对了\n", i+1)
+					n++
+				}
+			}
+			if n == 4 {
+				fmt.Println("恭喜你全部猜对了！")
+				break
+			}
 		} else {
-			fmt.Printf("第%d位数猜对了\n", i+1)
-			n++
+			fmt.Printf("输入的数字不符合要求，")
 		}
-	}
-	if n == 4 { // 全部才对
-		fmt.Println("")
-
 	}
 }
 
@@ -70,9 +65,6 @@ func main() {
 	s := make([]int, 4)
 
 	CreatNum(&randNum)
-	fmt.Println("randNum:", randNum)
-
 	GetNum(s, randNum)
-	fmt.Println(s)
 	OneGame(s)
 }
